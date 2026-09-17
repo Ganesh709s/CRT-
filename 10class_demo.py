087e5039dad7509acedee1f51be529c5c86420e1
Ganesh709s <s7ganesh6300@gmail.com>
2026-09-17 15:44:52 +0530

    10class_demo.py
    it's about addition and substraction

---
+++ 10class_demo.py
@@ -0,0 +1,9 @@
+class Demo:
+   def addition (self,a,b): 
+     print(a+b) 
+   def substraction (self a,b):
+     print (a*b) 
+     print(a*b)
+ d1=Demo()
+ d1.addition (10,20)
+ d1.subtraction (40,10)
